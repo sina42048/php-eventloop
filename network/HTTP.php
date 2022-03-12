fe1e407cdc6c21f49379fbe9a4c0ca56db85c899
sina42048 <sinafat76@gmail.com>
2022-03-12 10:51:16 +0100

    curl_multi_* way better than forked process for http requests

--- network/HTTP.php
+++ network/HTTP.php
@@ -2,104 +2,39 @@
 
 class HTTP
 {
-    public static $operations_holder = [];
-    public static $communicationPipes = [];
+    public static $multi_handlers = [];
 
-    public static function get($url, array $params)
+    public static function fetch($method, $url, array $params)
     {
-        return new Promise(function ($resolve, $reject) use ($url, &$params) {
-            self::httpRequest('GET', $url, $params, $resolve, $reject);
-        });
-    }
-
-    public static function post($url, array $params)
-    {
-        return new Promise(function ($resolve, $reject) use ($url, &$params) {
-            self::httpRequest('POST', $url, $params, $resolve, $reject);
-        });
-    }
-
-    public static function put($url, array $params)
-    {
-        return new Promise(function ($resolve, $reject) use ($url, &$params) {
-            self::httpRequest('PUT', $url, $params, $resolve, $reject);
-        });
-    }
-
-
-    public static function delete($url, array $params)
-    {
-        return new Promise(function ($resolve, $reject) use ($url, &$params) {
-            self::httpRequest('DELETE', $url, $params, $resolve, $reject);
+        return new Promise(function ($resolve, $reject) use (&$method, &$url, &$params) {
+            self::httpRequest($method, $url, $params, $resolve, $reject);
         });
     }
 
     private static function httpRequest($method, $url, array $params, $callback, $err)
     {
-        $random_number = rand(1, 10000);
-        $pipe_name = "/tmp/httpPipe" . $random_number;
-        posix_mkfifo($pipe_name, 0644);
-        $pid = pcntl_fork();
+        $multiHandlerAndCurlHandler = [];
 
-        if ($pid < 0) {
-            echo 'error';
-        } else if ($pid === 0) {
-            // child
-            error_reporting(0);
-
-            $pipe = fopen($pipe_name, "w");
-            stream_set_blocking($pipe, false);
-
-            switch ($method) {
-                case 'GET':
-                    $response = HTTPRequester::HTTPGet($url, $params);
-                    if (!$response) {
-                        fwrite($pipe, "HTTP_+_GET_+_FAILED_+_" . $response . "_+_" . PHP_EOL);
-                    } else {
-                        fwrite($pipe, "HTTP_+_GET_+_RESPONSE_+_" . $response . "_+_" . PHP_EOL);
-                    }
-                    break;
-                case 'POST':
-                    $response = HTTPRequester::HTTPPost($url, $params);
-                    if (!$response) {
-                        fwrite($pipe, "HTTP_+_POST_+_FAILED_+_" . $response . "_+_" . PHP_EOL);
-                    } else {
-                        fwrite($pipe, "HTTP_+_POST_+_RESPONSE_+_" . $response . "_+_" . PHP_EOL);
-                    }
-                    break;
-                case 'PUT':
-                    $response = HTTPRequester::HTTPPUT($url, $params);
-                    if (!$response) {
-                        fwrite($pipe, "HTTP_+_PUT_+_FAILED_+_" . $response . "_+_" . PHP_EOL);
-                    } else {
-                        fwrite($pipe, "HTTP_+_PUT_+_RESPONSE_+_" . $response . "_+_" . PHP_EOL);
-                    }
-                    break;
-                case 'DELETE':
-                    $response = HTTPRequester::HTTPDelete($url, $params);
-                    if (!$response) {
-                        fwrite($pipe, "HTTP_+_DELETE_+_FAILED_+_" . $response . "_+_" . PHP_EOL);
-                    } else {
-                        fwrite($pipe, "HTTP_+_DELETE_+_RESPONSE_+_" . $response . "_+_" . PHP_EOL);
-                    }
-                    break;
-            }
-
-            fclose($pipe);
-            unlink($pipe_name);
-
-            exit(0);
-        } else {
-            // parent
-            usleep(15);
-            $pipe = fopen($pipe_name, "r");
-            stream_set_blocking($pipe, false);
-
-            self::$communicationPipes[(int)$pipe] = $pipe;
-            self::$operations_holder[(int)$pipe] = [
-                'callback' => $callback,
-                'err' => $err,
-            ];
+        switch ($method) {
+            case 'GET':
+                $multiHandlerAndCurlHandler = HTTPRequester::HTTPGet($url, $params);
+                break;
+            case 'POST':
+                $multiHandlerAndCurlHandler = HTTPRequester::HTTPPost($url, $params);
+                break;
+            case 'PUT':
+                $multiHandlerAndCurlHandler = HTTPRequester::HTTPPut($url, $params);
+                break;
+            case 'DELETE':
+                $multiHandlerAndCurlHandler = HTTPRequester::HTTPDelete($url, $params);
+                break;
         }
+
+        self::$multi_handlers[] = [
+            'mh' => $multiHandlerAndCurlHandler[0],
+            'ch' => $multiHandlerAndCurlHandler[1],
+            'callback' => $callback,
+            'err' => $err
+        ];
     }
 }
--- network/HTTPRequester.php
+++ network/HTTPRequester.php
@@ -11,11 +11,17 @@ class HTTPRequester
     {
         $query = http_build_query($params);
         $ch = curl_init($url . '?' . $query);
+        curl_setopt($ch, CURLOPT_AUTOREFERER, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, false);
-        $response = curl_exec($ch);
-        curl_close($ch);
-        return $response;
+        curl_setopt($ch, CURLOPT_FAILONERROR, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        $mh = curl_multi_init();
+        curl_multi_add_handle($mh, $ch);
+        return [
+            $mh,
+            $ch
+        ];
     }
     /**
      * @description Make HTTP-POST call
@@ -27,14 +33,20 @@ class HTTPRequester
     {
         $query = http_build_query($params);
         $ch = curl_init();
+        curl_setopt($ch, CURLOPT_AUTOREFERER, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
-        $response = curl_exec($ch);
-        curl_close($ch);
-        return $response;
+        curl_setopt($ch, CURLOPT_FAILONERROR, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        $mh = curl_multi_init();
+        curl_multi_add_handle($mh, $ch);
+        return [
+            $mh,
+            $ch
+        ];
     }
     /**
      * @description Make HTTP-PUT call
@@ -46,14 +58,20 @@ class HTTPRequester
     {
         $query = \http_build_query($params);
         $ch = \curl_init();
+        curl_setopt($ch, CURLOPT_AUTOREFERER, true);
         \curl_setopt($ch, \CURLOPT_RETURNTRANSFER, true);
         \curl_setopt($ch, \CURLOPT_HEADER, false);
         \curl_setopt($ch, \CURLOPT_URL, $url);
         \curl_setopt($ch, \CURLOPT_CUSTOMREQUEST, 'PUT');
         \curl_setopt($ch, \CURLOPT_POSTFIELDS, $query);
-        $response = \curl_exec($ch);
-        \curl_close($ch);
-        return $response;
+        curl_setopt($ch, CURLOPT_FAILONERROR, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        $mh = curl_multi_init();
+        curl_multi_add_handle($mh, $ch);
+        return [
+            $mh,
+            $ch
+        ];
     }
     /**
      * @category Make HTTP-DELETE call
@@ -65,13 +83,19 @@ class HTTPRequester
     {
         $query = \http_build_query($params);
         $ch = \curl_init();
+        curl_setopt($ch, CURLOPT_AUTOREFERER, true);
         \curl_setopt($ch, \CURLOPT_RETURNTRANSFER, true);
         \curl_setopt($ch, \CURLOPT_HEADER, false);
         \curl_setopt($ch, \CURLOPT_URL, $url);
         \curl_setopt($ch, \CURLOPT_CUSTOMREQUEST, 'DELETE');
         \curl_setopt($ch, \CURLOPT_POSTFIELDS, $query);
-        $response = \curl_exec($ch);
-        \curl_close($ch);
-        return $response;
+        curl_setopt($ch, CURLOPT_FAILONERROR, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        $mh = curl_multi_init();
+        curl_multi_add_handle($mh, $ch);
+        return [
+            $mh,
+            $ch
+        ];
     }
 }
